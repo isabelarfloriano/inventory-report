7f393c731e43e7d6820cff4ad02a0c1a85312200
Isabela Rozani Floriano <irf161295@gmail.com>
2023-01-16 17:11:47 -0300

    fix: lint

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -2,7 +2,7 @@ from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    
+
     product = Product(
         1,
         "Produto Teste",
@@ -13,10 +13,12 @@ def test_cria_produto():
         "Mantenha fora do alcance de crianças",
     )
 
+    instructions = "Mantenha fora do alcance de crianças"
+
     assert product.id == 1
     assert product.nome_do_produto == "Produto Teste"
     assert product.nome_da_empresa == "Testers S.A"
     assert product.data_de_fabricacao == "01/01/1001"
     assert product.data_de_validade == "01/01/9999"
     assert product.numero_de_serie == "0123456789"
-    assert product.instrucoes_de_armazenamento == "Mantenha fora do alcance de crianças"
+    assert product.instrucoes_de_armazenamento == instructions
