762953b9befd1c49f21d260f8dd1a919827b0d94
Isabela Rozani Floriano <irf161295@gmail.com>
2023-01-16 17:02:10 -0300

    tests: create test for method init at class Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    
+    product = Product(
+        1,
+        "Produto Teste",
+        "Testers S.A",
+        "01/01/1001",
+        "01/01/9999",
+        "0123456789",
+        "Mantenha fora do alcance de crianças",
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "Produto Teste"
+    assert product.nome_da_empresa == "Testers S.A"
+    assert product.data_de_fabricacao == "01/01/1001"
+    assert product.data_de_validade == "01/01/9999"
+    assert product.numero_de_serie == "0123456789"
+    assert product.instrucoes_de_armazenamento == "Mantenha fora do alcance de crianças"
