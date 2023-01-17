de00431bf2724b354dd8d56f5dc93531b766beba
Isabela Rozani Floriano <irf161295@gmail.com>
2023-01-17 11:40:12 -0300

    feat: create static method to generate the complete report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,20 @@
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
 class CompleteReport:
-    pass
+    @staticmethod
+    def generate(list):
+        simple_report = SimpleReport.generate(list)
+
+        companies = [product["nome_da_empresa"] for product in list]
+        company_products = Counter(companies).items()
+        companies_amount = ""
+        for company, amount in company_products:
+            companies_amount += f"- {company}: {amount}\n"
+        
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{companies_amount}\n"
+        )
