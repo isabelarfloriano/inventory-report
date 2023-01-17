0a3adfe08eb2c8aff96f8adc69e71040542281c8
Isabela Rozani Floriano <irf161295@gmail.com>
2023-01-17 19:46:29 -0300

    fix: lint

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -9,16 +9,16 @@ import json
 class Inventory:
     @staticmethod
     def import_data(path, report_type):
-      if path.endswith(".csv"):
-        with open(path, encoding="utf-8") as csv_file:
-            file = csv.DictReader(csv_file)
-            products_file = [line for line in file]
-      
-      if path.endswith(".json"):
-        with open(path, encoding="utf-8") as json_file:
-            products_file = json.load(json_file)
+        if path.endswith(".csv"):
+            with open(path, encoding="utf-8") as csv_file:
+                file = csv.DictReader(csv_file)
+                products_file = [line for line in file]
 
-      if report_type == "simples":
-          return SimpleReport.generate(products_file)
-      elif report_type == "completo":
-          return CompleteReport.generate(products_file)
+        if path.endswith(".json"):
+            with open(path, encoding="utf-8") as json_file:
+                products_file = json.load(json_file)
+
+        if report_type == "simples":
+            return SimpleReport.generate(products_file)
+        elif report_type == "completo":
+            return CompleteReport.generate(products_file)
