0db0ca376eddceac570755640b06e1e9a8728174
Isabela Rozani Floriano <irf161295@gmail.com>
2023-01-17 19:45:56 -0300

    refactor: class inventory to generate reports via a JSON file too

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -3,16 +3,22 @@ from inventory_report.reports.complete_report import CompleteReport
 
 
 import csv
+import json
 
 
 class Inventory:
     @staticmethod
     def import_data(path, report_type):
+      if path.endswith(".csv"):
         with open(path, encoding="utf-8") as csv_file:
             file = csv.DictReader(csv_file)
             products_file = [line for line in file]
+      
+      if path.endswith(".json"):
+        with open(path, encoding="utf-8") as json_file:
+            products_file = json.load(json_file)
 
-            if report_type == "simples":
-                return SimpleReport.generate(products_file)
-            elif report_type == "completo":
-                return CompleteReport.generate(products_file)
+      if report_type == "simples":
+          return SimpleReport.generate(products_file)
+      elif report_type == "completo":
+          return CompleteReport.generate(products_file)
