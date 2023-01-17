0a5fcba3e1e6f7989e256d41188f5121034822e4
Isabela Rozani Floriano <irf161295@gmail.com>
2023-01-17 20:16:29 -0300

    refactor: class inventory to generate reports via a XML file too

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -4,6 +4,7 @@ from inventory_report.reports.complete_report import CompleteReport
 
 import csv
 import json
+import xmltodict
 
 
 class Inventory:
@@ -17,6 +18,11 @@ class Inventory:
         if path.endswith(".json"):
             with open(path, encoding="utf-8") as json_file:
                 products_file = json.load(json_file)
+
+        if path.endswith(".xml"):
+            with open(path) as xml_file:
+                file = xmltodict.parse(xml_file.read())
+                products_file = file["dataset"]["record"]
 
         if report_type == "simples":
             return SimpleReport.generate(products_file)
