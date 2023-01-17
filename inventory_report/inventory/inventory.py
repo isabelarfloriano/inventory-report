f5f6be2a28131d6c0f1cfb72e85c835b6f8f1bd8
Isabela Rozani Floriano <irf161295@gmail.com>
2023-01-17 18:52:07 -0300

    feat: generate reports via a CSV file

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,18 @@
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+import csv
+
+
+class Inventory:
+  @staticmethod
+  def import_data(path, report_type):
+    with open(path, encoding="utf-8") as csv_file:
+        file = csv.DictReader(csv_file)
+        products_file = [line for line in file]
+
+        if report_type == "simples":
+          return SimpleReport.generate(products_file)
+        elif report_type == "completo":
+          return CompleteReport.generate(products_file)
