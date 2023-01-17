12c3b8feb09b9248c0c7055b0a4768940c625704
Isabela Rozani Floriano <irf161295@gmail.com>
2023-01-17 19:17:26 -0300

    fix: lint

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -6,13 +6,13 @@ import csv
 
 
 class Inventory:
-  @staticmethod
-  def import_data(path, report_type):
-    with open(path, encoding="utf-8") as csv_file:
-        file = csv.DictReader(csv_file)
-        products_file = [line for line in file]
+    @staticmethod
+    def import_data(path, report_type):
+        with open(path, encoding="utf-8") as csv_file:
+            file = csv.DictReader(csv_file)
+            products_file = [line for line in file]
 
-        if report_type == "simples":
-          return SimpleReport.generate(products_file)
-        elif report_type == "completo":
-          return CompleteReport.generate(products_file)
+            if report_type == "simples":
+                return SimpleReport.generate(products_file)
+            elif report_type == "completo":
+                return CompleteReport.generate(products_file)
