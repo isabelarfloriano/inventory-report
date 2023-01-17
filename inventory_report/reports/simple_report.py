3245458bc393e7cec522796392b07a97a6429cc1
Isabela Rozani Floriano <irf161295@gmail.com>
2023-01-16 21:56:46 -0300

    fix: lint

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+# from datetime import datetime
 from collections import Counter
 
 
@@ -7,18 +7,18 @@ class SimpleReport:
     def generate(list):
         oldest_manufacture = "9999-99-99"
         for product in list:
-            if (oldest_manufacture > product["data_de_fabricacao"]): 
+            if oldest_manufacture > product["data_de_fabricacao"]:
                 oldest_manufacture = product["data_de_fabricacao"]
                 # print(oldest_manufacture)
 
         # now = datetime.today().strftime("%y-%m-%d")
         close_to_expire = "9999-99-99"
         for product in list:
-            if (close_to_expire > product["data_de_validade"]):
+            if close_to_expire > product["data_de_validade"]:
                 close_to_expire = product["data_de_validade"]
                 # print(close_to_expire)
 
-        companies = [ product["nome_da_empresa"] for product in list]
+        companies = [product["nome_da_empresa"] for product in list]
         company_with_more_products, _ = Counter(companies).most_common()[0]
         return (
             f"Data de fabricação mais antiga: {oldest_manufacture}\n"
