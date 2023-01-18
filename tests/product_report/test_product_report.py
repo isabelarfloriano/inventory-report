9bc3b922702d82c9676cc617ca417a6564269fd4
Isabela Rozani Floriano <irf161295@gmail.com>
2023-01-18 12:22:59 -0300

    tests: test the individual product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+
+    product = Product(
+        1,
+        "Produto Teste",
+        "Testers S.A",
+        "01/01/1001",
+        "01/01/9999",
+        "0123456789",
+        "Mantenha fora do alcance de crianças",
+    )
+
+    assert Product.__repr__(product) == str(
+        "O produto Produto Teste fabricado em 01/01/1001 "
+        "por Testers S.A com validade até 01/01/9999 "
+        "precisa ser armazenado Mantenha fora do alcance de crianças."
+    )
