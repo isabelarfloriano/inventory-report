251b6838fc10e140b190857f1c4eac30a6af13d2
Isabela Rozani Floriano <irf161295@gmail.com>
2023-01-17 21:06:29 -0300

    refactor: adapts class Inventory to use the Strategy pattern

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,29 +1,26 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
-
-
-import csv
-import json
-import xmltodict
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
 
 
 class Inventory:
     @staticmethod
     def import_data(path, report_type):
         if path.endswith(".csv"):
-            with open(path, encoding="utf-8") as csv_file:
-                file = csv.DictReader(csv_file)
-                products_file = [line for line in file]
+            products_file = CsvImporter.import_data(path)
 
-        if path.endswith(".json"):
-            with open(path, encoding="utf-8") as json_file:
-                products_file = json.load(json_file)
+        elif path.endswith(".json"):
+            products_file = JsonImporter.import_data(path)
 
-        if path.endswith(".xml"):
-            with open(path) as xml_file:
-                file = xmltodict.parse(xml_file.read())
-                products_file = file["dataset"]["record"]
+        elif path.endswith(".xml"):
+            products_file = XmlImporter.import_data(path)
 
+        return Inventory.generate_report_by_type(report_type, products_file)
+
+    @staticmethod
+    def generate_report_by_type(report_type, products_file):
         if report_type == "simples":
             return SimpleReport.generate(products_file)
         elif report_type == "completo":
