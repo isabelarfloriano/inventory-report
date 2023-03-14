88560b7e1c5b60e6fba6bf52717008bf8628105c
Isabela Rozani Floriano <irf161295@gmail.com>
2023-03-13 21:22:03 -0300

    feat: create iterator interface

---
+++ inventory_report/inventory/inventory_iterator.py
@@ -0,0 +1,16 @@
+from collections.abc import Iterator
+
+
+class InventoryIterator(Iterator):
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+
+    def __next__(self):
+        result = self.data[self.index]
+
+        if not result:
+            raise StopIteration()
+
+        self.index += 1
+        return InventoryIterator(self.data)
