bdf74d60ec8b4896c3ac8c31cce04ce164d669f6
Isabela Rozani Floriano <irf161295@gmail.com>
2023-03-13 21:44:34 -0300

    fix: inventoryrefactor logic

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -5,22 +5,17 @@ from inventory_report.reports.complete_report import CompleteReport
 
 class InventoryRefactor:
     def __init__(self, importer):
+        self.simples = SimpleReport
+        self.completo = CompleteReport
         self.importer = importer
         self.data = []
 
-    def import_data(self, path, report_type):
-        products_file = self.importer.import_data(path)
-        self.data += products_file
-
-        return InventoryRefactor.generate_report_by_type(
-            report_type, products_file)
-
-    @staticmethod
-    def generate_report_by_type(report_type, products_file):
-        if report_type == "simples":
-            return SimpleReport.generate(products_file)
-        elif report_type == "completo":
-            return CompleteReport.generate(products_file)
+    def import_data(self, file, type):
+        self.data.extend(self.importer.import_data(file))
+        if type == "simples":
+            return self.simples.generate(self.data)
+        elif type == "completo":
+            return self.completo.generate(self.data)
 
     def __iter__(self):
         return InventoryIterator(self.data)
