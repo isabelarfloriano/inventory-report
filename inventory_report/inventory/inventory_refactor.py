89833a7e5367f3bf856e8e5dbe7afb1c12774e1e
Isabela Rozani Floriano <irf161295@gmail.com>
2023-03-13 21:19:45 -0300

    feat: create refactor version of inventory

---
+++ inventory_report/inventory/inventory_refactor.py
@@ -0,0 +1,22 @@
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class InventoryRefactor:
+    def __init__(self, importer):
+        self.importer = importer
+        self.data = []
+
+    def import_data(self, path, report_type):
+        products_file = self.importer.import_data(path)
+        self.data += products_file
+
+        return InventoryRefactor.generate_report_by_type(
+            report_type, products_file)
+
+    @staticmethod
+    def generate_report_by_type(report_type, products_file):
+        if report_type == "simples":
+            return SimpleReport.generate(products_file)
+        elif report_type == "completo":
+            return CompleteReport.generate(products_file)
