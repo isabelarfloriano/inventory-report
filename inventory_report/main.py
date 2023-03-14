4d37b49eb61ea21e4ff22f4e7b28d53c46124368
Isabela Rozani Floriano <irf161295@gmail.com>
2023-03-13 21:37:21 -0300

    feat: create method main for refactor inventory reports

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,3 +1,29 @@
+import sys
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
+
 def main():
-    # init
-    pass
+    if len(sys.argv) < 3:
+        sys.stderr.write("Verifique os argumentos\n")
+        return
+
+    path = sys.argv[1]
+    report_type = sys.argv[2]
+
+    if path.endswith(".csv"):
+        importer = CsvImporter()
+    elif path.endswith(".json"):
+        importer = JsonImporter()
+    elif path.endswith(".xml"):
+        importer = XmlImporter()
+    else:
+        sys.stderr.write("Tipo de arquivo não suportado\n")
+        return
+
+    inventory = InventoryRefactor(importer)
+    report = inventory.import_data(path, report_type)
+
+    print(report)
